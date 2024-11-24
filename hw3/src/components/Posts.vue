08a72ff040566c15724b8c946e684d428e3b37ad
OskarMannik <mannikoskar@gmail.com>
2024-11-24 23:09:29 +0200

    commit

--- hw3/src/components/Posts.vue
+++ hw3/src/components/Posts.vue
@@ -2,31 +2,43 @@
     <div>
       <div v-for="post in posts" :key="post.id" class="post">
         <div class="post-header">
-          <img src="logo.jpg" alt="User Profile" class="profile-image" />
+          <img src="images\logo.jpg" alt="User Profile" class="profile-image" />
           <span class="post-author">{{ post.author }}</span>
           <span class="post-date">{{ formatDate(post.createTime) }}</span>
         </div>
         <img v-if="post.image" :src="post.image" alt="Post Image" class="post-image" />
         <p class="post-text">{{ post.content }}</p>
         <div class="post-footer">
-          <span class="like-button">👍 Like</span>
+          <button class="like-button" @click="likePost(post.id)">
+            👍 Like 
+          </button>
+          {{ post.likes }}  
         </div>
       </div>
     </div>
+    <button @click="resetLikes" class="reset-button">Reset Likes</button>
   </template>
   
   <script>
-  import { mapGetters } from "vuex";
+  import { mapGetters, mapMutations } from "vuex";
   
   export default {
-    name: "PostsComponent", // Combined component
+    name: "PostsComponent",
     computed: {
       ...mapGetters(["allPosts"]),
       posts() {
-        return this.allPosts;
+        return this.allPosts; // Get posts from Vuex
       },
     },
     methods: {
+        ...mapMutations(["incrementLikes"]),
+        ...mapActions(["resetAllLikes"]),
+        likePost(postId) {
+      this.incrementLikes(postId);
+    },
+      resetLikes() {
+      this.resetAllLikes();
+    },
       formatDate(dateString) {
         const date = new Date(dateString);
         return date.toLocaleDateString("en-US", {
@@ -70,6 +82,22 @@
   .like-button {
     cursor: pointer;
     color: #007bff;
+    background-color: transparent;
+    border: none;
+    font-size: 16px;
   }
+  .reset-button {
+  margin-bottom: 1rem;
+  padding: 0.5rem 1rem;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 4px;
+}
+.reset-button:hover {
+  background-color: #0056b3;
+}
   </style>
   
